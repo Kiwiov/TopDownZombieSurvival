3dcb4067582fd9a0afd549b5a86119529d11056f
kiwiov <hugohansen97@gmail.com>
2016-10-14 11:25:23 +0200

    ...

--- ZombieSurvival/ZombieSurvival/Game1.cs
+++ ZombieSurvival/ZombieSurvival/Game1.cs
@@ -24,25 +24,26 @@ namespace ZombieSurvival
         Vector2 mousePos;
         Texture2D curs;
         float rot;
-        
-
+        float stamina;
+        Camera cam;
+        Vector2 mousePosition;
         public Game1()
         {
             
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
             
+            graphics.IsFullScreen = true;
         }
 
         
         protected override void Initialize()
         {
-            position = new Vector2(300,300);
-            
-
-
-
-            
+            position = new Vector2(1600,1300);
+            cam = new Camera();
+            mousePosition = new Vector2();
             base.Initialize();
         }
 
@@ -52,7 +53,7 @@ namespace ZombieSurvival
             map = new TmxMap("house.tmx");
             tileEngineGood = new TileEngineGood(map);
             tileEngineGood.LoadContent(this);
-            character = Content.Load<Texture2D>("manBlue_gun");
+            character = Content.Load<Texture2D>("hitman2_gun");
             curs = Content.Load<Texture2D>("crshair_36px");
         }
 
@@ -88,44 +89,122 @@ namespace ZombieSurvival
 
             KeyboardState kb = Keyboard.GetState();
 
-            if (kb.IsKeyDown(Keys.A) || kb.IsKeyDown(Keys.Left))
+            if ((kb.IsKeyDown(Keys.LeftShift) || kb.IsKeyDown(Keys.RightShift)) && stamina > 0)
             {
-                position.X = position.X - 5;
-            }
+                if ((kb.IsKeyDown(Keys.A) || kb.IsKeyDown(Keys.Left)) && (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Up)))
+                {
+                    position.X = position.X - 3.5355339059327376220042218105242f;
+                    position.Y = position.Y - 3.5355339059327376220042218105242f;
+                }
+                else if ((kb.IsKeyDown(Keys.D) || kb.IsKeyDown(Keys.Right)) && (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Up)))
+                {
+                    position.X = position.X + 3.5355339059327376220042218105242f;
+                    position.Y = position.Y - 3.5355339059327376220042218105242f;
+                }
+                else if ((kb.IsKeyDown(Keys.A) || kb.IsKeyDown(Keys.Left)) && (kb.IsKeyDown(Keys.S) || kb.IsKeyDown(Keys.Down)))
+                {
+                    position.X = position.X - 3.5355339059327376220042218105242f;
+                    position.Y = position.Y + 3.5355339059327376220042218105242f;
+                }
+                else if ((kb.IsKeyDown(Keys.D) || kb.IsKeyDown(Keys.Right)) && (kb.IsKeyDown(Keys.S) || kb.IsKeyDown(Keys.Down)))
+                {
+                    position.X = position.X + 3.5355339059327376220042218105242f;
+                    position.Y = position.Y + 3.5355339059327376220042218105242f;
+                }
+                else if (kb.IsKeyDown(Keys.A) || kb.IsKeyDown(Keys.Left))
+                {
+                    position.X = position.X - 5;
+                }
 
-            if (kb.IsKeyDown(Keys.D) || kb.IsKeyDown(Keys.Right))
-            {
-                position.X = position.X + 5;
+                else if (kb.IsKeyDown(Keys.D) || kb.IsKeyDown(Keys.Right))
+                {
+                    position.X = position.X + 5;
+                }
+
+                else if (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Up))
+                {
+                    position.Y = position.Y - 5;
+                }
+                else if (kb.IsKeyDown(Keys.S) || kb.IsKeyDown(Keys.Down))
+                {
+                    position.Y = position.Y + 5;
+                }
+
+                stamina -= 5;
             }
-            if (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Up))
+            else
             {
-                position.Y = position.Y - 5;
-            }
-            if (kb.IsKeyDown(Keys.S) || kb.IsKeyDown(Keys.Down))
-            {
-                position.Y = position.Y + 5;
+                if (stamina <= 25 * 60)
+                {
+                    stamina += 1;
+                }
+                if ((kb.IsKeyDown(Keys.A) || kb.IsKeyDown(Keys.Left)) && (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Up)))
+                {
+                    position.X = position.X - 2.1213203435596425732025330863145f;
+                    position.Y = position.Y - 2.1213203435596425732025330863145f;
+                }
+                else if ((kb.IsKeyDown(Keys.D) || kb.IsKeyDown(Keys.Right)) && (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Up)))
+                {
+                    position.X = position.X + 2.1213203435596425732025330863145f;
+                    position.Y = position.Y - 2.1213203435596425732025330863145f;
+                }
+                else if ((kb.IsKeyDown(Keys.A) || kb.IsKeyDown(Keys.Left)) && (kb.IsKeyDown(Keys.S) || kb.IsKeyDown(Keys.Down)))
+                {
+                    position.X = position.X - 2.1213203435596425732025330863145f;
+                    position.Y = position.Y + 2.1213203435596425732025330863145f;
+                }
+                else if ((kb.IsKeyDown(Keys.D) || kb.IsKeyDown(Keys.Right)) && (kb.IsKeyDown(Keys.S) || kb.IsKeyDown(Keys.Down)))
+                {
+                    position.X = position.X + 2.1213203435596425732025330863145f;
+                    position.Y = position.Y + 2.1213203435596425732025330863145f;
+                }
+                else if (kb.IsKeyDown(Keys.A) || kb.IsKeyDown(Keys.Left))
+                {
+                    position.X = position.X - 3;
+                }
+
+                else if (kb.IsKeyDown(Keys.D) || kb.IsKeyDown(Keys.Right))
+                {
+                    position.X = position.X + 3;
+                }
+
+                else if (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Up))
+                {
+                    position.Y = position.Y - 3;
+                }
+                else if (kb.IsKeyDown(Keys.S) || kb.IsKeyDown(Keys.Down))
+                {
+                    position.Y = position.Y + 3;
+                }
             }
             
 
+            
 
+            
 
+            cam.Pos = position;
             mousePos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
-            dPos = mousePos - position;
-            rot = (float) Math.Atan2(dPos.Y, dPos.X);
-
-
-            
+            mousePosition = new Vector2(mousePos.X, mousePos.Y) + cam.pos - new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+            dPos = mousePosition - position;
+            rot = (float)Math.Atan2(dPos.Y, dPos.X);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.TransparentBlack);
 
-            spriteBatch.Begin();
+            spriteBatch.Begin(SpriteSortMode.Deferred,
+                                BlendState.AlphaBlend,
+                                SamplerState.LinearWrap,
+                                DepthStencilState.Default,
+                                RasterizerState.CullNone,
+                                null,
+                                cam.get_transformation(GraphicsDevice));
             tileEngineGood.Draw(spriteBatch);
             spriteBatch.Draw(character, position, null, color:Color.White, rotation:rot, origin: new Vector2(character.Bounds.Center.X, character.Bounds.Center.Y));
-            spriteBatch.Draw(curs, mousePos, Color.White);
+            spriteBatch.Draw(curs, mousePosition, Color.GreenYellow);
             spriteBatch.End();
 
             base.Draw(gameTime);
